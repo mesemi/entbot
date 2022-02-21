2f4b34861625ddc1af6d0b1f23d2b964cfd5f107
mesemi <mesemi@mesemi.net>
2022-02-21 13:56:32 -0500

    test 3?

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ client.on('interactionCreate', async interaction => {
 client.on('guildMemberRemove', async member => {
   var data = require('/app/.data/data.json')
   console.log("occured")
-  delete data.users[member.id]
+  delete data.users[member.id];
   fs.writeFile('/app/.data/data.json', JSON.stringify(data), function (err) { if (err) throw err;});
 })
 
