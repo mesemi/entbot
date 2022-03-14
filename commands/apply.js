797a47b5dc93a21f179a99016cb774e05a315426
mesemi <mesemi@mesemi.net>
2022-03-14 13:06:50 -0400

    starting application system

---
+++ commands/apply.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageButton } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('apply')
+		.setDescription('Starts the E&T application.')
+		.addStringOption(option =>
+				option.setName("test")
+							.setDescription("test")
+							.setRequired(true)),
+	async execute(client, interaction) {
+		const { MessageEmbed } = require('discord.js');
+		const test = interaction.options.getString("test")
+
+		const row = new MessageActionRow()
+				.addComponents(
+					new MessageButton()
+						.setCustomId('testaccept')
+						.setLabel('Accept')
+						.setStyle('SUCCESS'),
+					new MessageButton()
+						.setCustomId('testdeny')
+						.setLabel('Deny')
+						.setStyle('DANGER')
+				);
+
+				const theEmbed = new MessageEmbed()
+						.setColor('#233287')
+						.setTitle(interaction.member.displayName + "'s Test")
+						.setAuthor(interaction.member.displayName, interaction.user.avatarURL())
+						.setDescription(test)
+						.setTimestamp()
+
+				interaction.reply({ embeds: [theEmbed], components: [row] })
+
+
+	},
+};
